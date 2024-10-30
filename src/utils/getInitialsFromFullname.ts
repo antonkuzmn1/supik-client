b862f15cba579ec4a86a39e0f3d84ee51ce897fc
anton <antonkuzmn1@gmail.com>
2024-10-30 19:23:49 +0700

    24.14.3: Added support for double surnames

--- src/utils/getInitialsFromFullname.ts
+++ src/utils/getInitialsFromFullname.ts
@@ -2,13 +2,24 @@ export const getInitialsFromFullname = (fullname: string) => {
     try {
         const trim = fullname.trim();
         const split = trim.split(' ');
-        const surname = split[0];
-        const name = split[1];
-        const patronymic = split[2];
-        const nameChar = name.charAt(0);
-        const patronymicChar = patronymic.charAt(0);
 
-        return `${surname} ${nameChar}. ${patronymicChar}.`
+        if (split.length > 3) {
+            const surname = split[0];
+            const name = split[2];
+            const patronymic = split[3];
+            const nameChar = name.charAt(0);
+            const patronymicChar = patronymic.charAt(0);
+
+            return `${surname} ${nameChar}. ${patronymicChar}.`
+        } else {
+            const surname = split[0];
+            const name = split[1];
+            const patronymic = split[2];
+            const nameChar = name.charAt(0);
+            const patronymicChar = patronymic.charAt(0);
+
+            return `${surname} ${nameChar}. ${patronymicChar}.`
+        }
     } catch (error) {
         return '';
     }
